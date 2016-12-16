f9280d254e3232c730b7ce93224586faba75af37
David Elleneberger <ellenb1@stolaf.edu>
2016-12-15 21:09:14 -0600

    new splash page, working on css and jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Needleman-Wunsch Online' });
 });
 
 module.exports = router;
